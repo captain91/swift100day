0a99f25019ed3cf1cf7fcb7d9f4789bf20fb9763
Shibo Sun <captain0091@163.com>
2021-02-22 13:43:58 +0800

    4 day

--- Swift100day/Swift100day/ViewController.swift
+++ Swift100day/Swift100day/ViewController.swift
@@ -128,6 +128,127 @@ class ViewController: UIViewController {
         default:
             print("Enjoy your day!")
         }
+        
+        let count = 1...10
+        for number in count {
+            print("Number is \(number)")
+        }
+        
+        let albums = ["Red", "1989", "Reputataion"]
+//        albums.shuffled()
+        for album in albums {
+            print("\(album) is on Apple Music")
+        }
+        
+        for _ in 1...4{
+            print("for loops")
+        }
+        
+        var number1 = 1
+        while number1 <= 20 {
+            print(number1)
+            number1 += 1
+        }
+        print("Ready or not, here I come!")
+        
+        var b: Int = 10
+        while b > 0 {
+            b -= 2
+            if b % 2 == 0 {
+                print("\(b) is an even number.")
+            }
+        }
+        
+        var averageScore = 2.5
+        while averageScore < 15.0 {
+            averageScore += 2.5
+            print("The average score is \(averageScore)")
+        }
+        
+        var speed = 50
+        while speed <= 55 {
+            speed += 1
+            print("Accelerating to \(speed)")
+        }
+        
+        var nn = 1
+        repeat {
+            print(nn)
+            nn += 1
+        }while nn <= 10
+        
+        
+        let nns = [1,2,3,4,5]
+        var random: [Int]
+        
+        repeat {
+            print("random")
+            random = nns.shuffled()
+        }while random == nns
+        
+        let numbers1 = [1, 2, 3, 4, 5]
+        var random1 = numbers1.shuffled()
+
+        while random1 == numbers1 {
+            random1 = numbers1.shuffled()
+            print("while")
+        }
+        
+        outerLoop: for i in 1...10{
+            for j in 1...10{
+                let product = i * j
+                print("\(i) * \(j) is \(product)")
+                
+                if product == 50 {
+                    print("It's a bullseye!")
+                    break outerLoop
+                }
+            }
+            print("outerLoop")
+        }
+        let options = ["up", "down", "left", "right"]
+        let secretCombination = ["up", "up", "right"]
+        outerLoop: for option1 in options {
+            for option2 in options {
+                for option3 in options {
+                    print("In loop")
+                    let attempt = [option1, option2, option3]
+
+                    if attempt == secretCombination {
+                        print("The combination is \(attempt)!")
+                        break outerLoop
+                    }
+                }
+            }
+        }
+        
+        for i in 1...10 {
+            if i % 2 == 1{
+                continue
+            }
+            print(i)
+        }
+        
+        let fibonacci = [1, 1, 2, 3, 5, 8, 13, 21]
+        var position = 0
+        while position <= 7 {
+            let value = fibonacci[position]
+            position += 1
+            if value < 2 {
+               continue
+            }
+            print("Fibonacci number \(position) is \(value)")
+        }
+        
+        var counting = 0
+        while counting <= 20 {
+            counting += 1
+            if counting > 5 {
+                continue
+            }
+            print("\(counting)...")
+        }
+        print("Ready or not, here I come!")
         // Do any additional setup after loading the view.
     }
 
