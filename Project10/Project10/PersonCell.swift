e760f9ea180a2aacd2fa76d7b93f6563dd0339ae
Shibo Sun <captain0091@163.com>
2021-03-26 11:10:58 +0800

    day 42

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Shibo Sun on 2021/3/26.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
