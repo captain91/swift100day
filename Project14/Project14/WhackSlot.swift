38b4a0a1e3c34aec59bbfaf88d965f15def8cfee
Shibo Sun <captain0091@163.com>
2021-04-08 16:49:51 +0800

    day 55

--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -35,9 +35,17 @@ class WhackSlot: SKNode {
     func show(hideTime: Double){
         if isVisible { return }
         
+        charNode.xScale = 1
+        charNode.yScale = 1
+        
         charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
         isVisible = true
         isHit = false
+        
+//        if let fireParticles = SKEmitterNode(fileNamed: "smoke"){
+//            fireParticles.position = CGPoint(x: charNode.position.x, y: charNode.position.y + 80)
+//            addChild(fireParticles)
+//        }
         
         if Int.random(in: 0...2) == 0{
             charNode.texture = SKTexture(imageNamed: "penguinGood")
@@ -58,4 +66,17 @@ class WhackSlot: SKNode {
         charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
         isVisible = false
     }
+    
+    func hit(){
+        isHit = true
+        
+        let delay = SKAction.wait(forDuration: 0.25)
+        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run {
+            [weak self] in self?.isVisible = false
+        }
+        let sequence = SKAction.sequence([delay, hide, notVisible])
+        charNode.run(sequence)
+    }
+    
 }
